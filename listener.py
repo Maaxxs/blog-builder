2f69776d2aec185948c18c638375e5bfa0f4d0e9
Max Kunzelmann <maxdev@posteo.de>
2020-11-15 23:42:40 +0100

    Improve logging and calling the actual build script

--- listener.py
+++ listener.py
@@ -1,18 +1,37 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request
 from hashlib import sha256
 import hmac
+import logging
+import subprocess
 
 app = Flask(__name__)
 # config
 app.config.from_object("config")
 
+# Logging to file
+logging.basicConfig(
+    filename=app.config["LOGFILE"],
+    format="[%(asctime)s] [%(levelname)s] %(message)s",
+    level=logging.DEBUG,
+)
+
+
 try:
     app.config.from_envvar("PROD_APP_SETTINGS")
+    api.logger.info("Production settings loaded")
 except:
-    #  print("[INFO]: Env variable PROD_APP_SETTINGS not set. Using default settings")
-    app.logger.info(
-        "Env variable PROD_APP_SETTINGS not set. Using default settings\nDo NOT use in production."
+    app.logger.warning(
+        "Do NOT use in production! Env variable PROD_APP_SETTINGS not set and defaults are used."
     )
+
+
+"""
+Handles the call of new push events.
+Looks for the event and signature headers
+and verifies the signature with the secret API token.
+
+Returns always just an empty string.
+"""
 
 
 @app.route("/api/github/push", methods=["POST"])
@@ -20,6 +39,14 @@ def push_received():
     try:
         github_signature = request.headers.get("X-Hub-Signature-256")
         github_event = request.headers.get("X-Github-Event")
+
+        if github_signature is None or github_event is None:
+            # Defenitely not a github api call and I dont care
+            # about good information for the sender.
+            app.logger.info(
+                f"Github event or signature not found.\nEvent: {github_event}\nSignature: {github_signature}"
+            )
+            return ""
 
         app.logger.info(f"Github signature: {github_signature}")
         app.logger.info(f"Github event: {github_event}")
@@ -33,7 +60,17 @@ def push_received():
 
         # secure compare
         if hmac.compare_digest(github_signature, signature):
-            print("Signatures match!")
+            app.logger.info("Signature validation successful")
+
+            with open(app.config["BUILD_SCRIPT_LOG"], "w") as outputfile:
+                # pipe stderr to stdout and capture this in the log file
+                subprocess.run(
+                    app.config["BUILD_SCRIPT"],
+                    stdout=outputfile,
+                    stderr=subprocess.STDOUT,
+                )
+                # TODO: Maybe send the logfile via email.
+
         else:
             app.logger.error(
                 f"Signatures do not match\nGithub: {github_signature}\n  Ours: {signature}"
